c33a98ed9af7fb0062dddedb836127e78483316c
ajit-jain <jainajit194@gmail.com>
2019-02-10 23:23:21 +0530

    assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { SocialAppService } from './shared/services/socail_app.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+declare var window: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor(private _socialAppService: SocialAppService,
+    public route: ActivatedRoute,
+  ) { }
+  ngOnInit() {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { ContactInfoService } from './shared/services/contact-info.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocialAppService } from 'src/app/shared/services/socail_app.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { LinkedInLoginComponent } from './components/linked-in-login/linked-in-login.component';
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormulaCalComponent } from './components/formula-cal/formula-cal.component';
+const routes = [
+  { path: 'linkedIn-login', component: LinkedInLoginComponent },
+  { path: 'contact-info', component: ContactInfoComponent },
+  { path: 'formula-calculator', component: FormulaCalComponent }
 
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkedInLoginComponent,
+    ContactInfoComponent,
+    FormulaCalComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SocialAppService, ContactInfoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
